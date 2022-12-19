052031198b92b794493e975e593f04ac42f0081e
Andres Parra <andeveling@gmail.com>
2022-12-18 20:57:36 -0500

    fix/deploy

--- src/redux/apiSlice.ts
+++ src/redux/apiSlice.ts
@@ -1,8 +1,10 @@
 import { RootState } from "@/redux"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
+const dev = process.env.NODE_ENV !== "production"
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3001",
+  baseUrl: dev ? "http://localhost:3001" : "http://localhost:3001",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token
     if (token) {
